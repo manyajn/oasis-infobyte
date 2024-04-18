ef0e18158d8553599a16ea4cd26e6c7345fc495b
manyajn <122954590+manyajn@users.noreply.github.com>
2024-04-18 19:44:56 +0530

    landing page

---
+++ med.js
@@ -0,0 +1,8 @@
+
+ let btn =document.querySelector('#btun');
+ btn.addEventListener("click",function()
+ {
+   let input = prompt("enter the timings");
+   alert("Your Booking is successfull at timings: " + input);
+   
+ });
